1e1a83d65e92624d2bdd57a6f4b70078ba105977
Vincent Ricard <ricard@intechinfo.fr>
2015-07-16 10:04:54 +0200

    Added DForm (+ base code)
    Added QuestionBase

---
+++ D-Form.Library/DForm.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D_Form.Library
+{
+    public sealed class DForm
+    {
+        private readonly DateTime _created;
+        private readonly List<QuestionBase> _questions;
+        
+        public String Title { get; set; }
+        public String Author { get; set; }
+        public int Version { get; private set; }
+
+        public DateTime Created { get { return _created; } }
+        public DateTime LastModified { get; private set; }
+
+        public List<QuestionBase> Questions { get { return _questions; } }
+
+        public DForm(string title, string author)
+        {
+            if( String.IsNullOrEmpty( title ) )
+                throw new ArgumentException( "title", "title MUST NOT be NULL or EMPTY!" );
+            if( String.IsNullOrEmpty( title ) )
+                throw new ArgumentException( "author", "author MUST NOT be NULL or EMPTY!" );
+
+            _created = DateTime.UtcNow;
+            LastModified = _created;
+            Version = 0;
+            Title = title;
+            Author = author;
+            _questions = new List<QuestionBase>();
+        }
+
+        public DForm()
+            : this( "Undefined", "Undefined" )
+        {
+        }
+    }
+}
--- D-Form.Library/QuestionBase.cs
+++ D-Form.Library/QuestionBase.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace D_Form_Library
+namespace D_Form.Library
 {
-    public class Class1
+    public abstract class QuestionBase
     {
     }
 }
