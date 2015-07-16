4daab497cf97a077d191c6a8150f9256079e1129
BenjaminGratade <gratade@intechinfo.fr>
2015-07-16 12:22:55 +0200

    MultipleChoiceQuestion add Choices

--- D-Form.Library/MultipleChoiceQuestion.cs
+++ D-Form.Library/MultipleChoiceQuestion.cs
@@ -8,5 +8,32 @@ namespace D_Form.Library
 {
     public sealed class MultipleChoiceQuestion
     {
+        private List<string> _choices;
+
+        public List<string> Choices
+        {
+            get { return _choices; }
+            set { _choices = value; }
+        }
+
+        public bool AddChoice(string newChoice)
+        {
+            if (!_choices.Contains(newChoice))
+            {
+                _choices.Add(newChoice);
+                return true;
+            }
+            else { return false; }
+        }
+
+        public bool RemoveChoice(string Choice)
+        {
+            if (_choices.Contains(Choice))
+            {
+                _choices.Remove(Choice);
+                return true;
+            }
+            else { return false; }
+        }
     }
 }
